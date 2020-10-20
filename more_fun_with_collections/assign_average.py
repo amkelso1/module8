cc1882e318e594452463a956684417f92902221d
amkelso1 <amkelso1@dmacc.edu>
2020-10-20 18:43:34 -0500

    wrote empty function, test failed

---
+++ more_fun_with_collections/assign_average.py
@@ -0,0 +1,11 @@
+"""
+Author
+"""
+
+
+def switch_average(entry):
+    pass
+
+
+if __name__ == '__main__':
+    switch_average()
---
+++ test/test_assign_average.py
@@ -0,0 +1,11 @@
+import unittest
+from more_fun_with_collections import assign_average
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(assign_average.switch_average(entry='A'), )
+
+
+if __name__ == '__main__':
+    unittest.main()
