bb618ea9fbbcdc39d922044d9e41cb00076f99b7
amkelso1 <amkelso1@dmacc.edu>
2020-10-20 19:05:02 -0500

    wrote grade keys and values in dictionary, tests all passed

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -3,9 +3,24 @@ Author
 """
 
 
+def deans_list():
+    return 'Welcome to the dean\'s list'
+
+
+def grade():
+    return 'Congratulations om receiving your final grade!'
+
+
 def switch_average(entry):
-    pass
+    my_dict = {'A': deans_list(),
+               'B': grade(),
+               'C': grade(),
+               'D': grade(),
+               'F': grade()}
+    result = my_dict.get(entry, lambda: 'This is not a valid key')
+    return result
 
 
 if __name__ == '__main__':
-    switch_average()
+    my_function = switch_average('A')
+    print(my_function)
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -3,8 +3,23 @@ from more_fun_with_collections import assign_average
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(assign_average.switch_average(entry='A'), )
+    def test_a_grade(self):
+        self.assertEqual(assign_average.switch_average(entry='A'), 'Welcome to the dean\'s list')
+
+    def test_b_grade(self):
+        self.assertEqual(assign_average.switch_average(entry='B'), 'Congratulations om receiving your final grade!')
+
+    def test_c_grade(self):
+        self.assertEqual(assign_average.switch_average(entry='C'), 'Congratulations om receiving your final grade!')
+
+    def test_d_grade(self):
+        self.assertEqual(assign_average.switch_average(entry='D'), 'Congratulations om receiving your final grade!')
+
+    def test_f_grade(self):
+        self.assertEqual(assign_average.switch_average(entry='F'), 'Congratulations om receiving your final grade!')
+
+    def test_not_grade(self):
+        self.assertNotEqual(assign_average.switch_average(entry='E'), 'Congratulations om receiving your final grade!')
 
 
 if __name__ == '__main__':
